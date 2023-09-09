577c07ba6f66d34cb563c08993a8b48ff10efa11
ashleycolton <livingsanity@gmail.com>
2023-09-10 06:23:45 +0800

    added content

---
+++ ori/Improvised.js
@@ -0,0 +1,163 @@
+function hasBlacklistKeywords(bio) {
+	const blacklist = [
+		'ladyboy',
+		//'banci',
+		//'bencong',
+		'lady boy',
+		//'not a lady',
+		//'not lady',
+		'not a girl',
+		'not girl',
+		'trans',
+		'shemale',
+		//'chubby',
+		//' lb ',
+	];
+
+	for (item of blacklist) {
+		if (bio.toLowerCase().indexOf(item) !== -1) {
+			console.log('skipping profile, matched blacklist keyword ' + item);
+			return true;
+		}
+	}
+	for (let i = 0; i < blacklist.length; i++) {
+		if (name.toLowerCase().indexOf(blacklist[i]) !== -1) {
+		  console.log('skipping profile, matched blacklist keyword ' + blacklist[i]);
+		  return true;
+		}
+	  }
+	  return false;
+	}
+	
+	function hasChineseOrThaiCharacters(name) {
+	  const chineseRegex = /[\u4e00-\u9fa5]/;
+	  const thaiRegex = /[\u0e00-\u0e7f]/;
+	  return chineseRegex.test(name) || thaiRegex.test(name);
+	}
+	
+	function filterNames(names) {
+		return names.filter(name => !hasBlacklistKeywords(name) && !hasChineseOrThaiCharacters(name));
+	  }
+  
+	  // Check for Thai and Chinese characters
+	  function hasThaiOrChineseCharacters(bio) {
+		const thaiRegex = /[\u0E00-\u0E7F]/;
+		const chineseRegex = /[\u4E00-\u9FAF]/;
+  
+		if (thaiRegex.test(bio) || chineseRegex.test(bio)) {
+		  console.log('skipping profile, matched Thai or Chinese characters');
+		  return true;
+		}
+  
+		return false;
+	  }
+
+function hasValidProfile() {
+	try {
+		const bioContainer = document.querySelector('.profileCard .profileContent .profileCard__card .BreakWord');
+		if (!bioContainer) return true;
+		const bio = bioContainer.textContent;
+		const nameContainer = document.querySelector('.profileCard .profileContent .profileCard__card .Fz($xl)');
+		if (!nameContainer) return true;
+		const name = nameContainer.textContent.trim();
+		console.log(bio);
+		console.log(name);
+		return !hasBlacklistKeywords(bio) && !hasBlacklistKeywords(name) && !hasChineseOrThaiCharacters(name);
+	} catch (e) {
+		// console.log(e);
+		return true; // possible empty bio
+	}
+}
+
+function checkTinder() {
+	const base = "https://tinder.com/";
+	return window.location.href.startsWith(base + "app/recs") || window.location.href.startsWith(base + "app/matches");
+}
+
+function isMatch() {
+	return document.querySelector('a.active');
+}
+
+// prevent async execution
+function pause(milliseconds) {
+	const dt = new Date();
+	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
+}
+
+function trickTinder() {
+	const infoClassName = 'focus-button-style';
+	const nbButtons = document.getElementsByClassName("button").length;
+	const buttons = document.getElementsByClassName("button");
+
+	const dislike = nbButtons === 5 ? buttons[1] : buttons[0];
+	const like = nbButtons === 5 ? buttons[3] : buttons[2];
+	
+	// Open profile bio
+	const info = document.getElementsByClassName(infoClassName)[0];
+	if (info) {
+		info.click();
+	}
+	pause(600);
+
+	// Like or deslike depending on validation
+	if (hasValidProfile()) {
+		like.click();
+
+		const thereIsMatch = isMatch();
+		if (thereIsMatch) {
+			console.log('------------- IT\'S A MATCH ! -------------');
+			thereIsMatch.click();
+		}
+	} else {
+		dislike.click();
+	}
+
+	// If reached max likes per day then show modal and get it's content...
+	// Check if there is any subscription button...
+	if (document.getElementsByClassName('productButton__subscriptionButton').length > 0) {
+		// We get the counter thing
+		const hms = document.getElementsByClassName('Fz($ml)')[0].textContent;
+		// Split it at the colons
+		const a = hms.split(':');
+		// Minutes are worth 60 seconds. Hours are worth 60 minutes. 1 second = 1kmilliseconds.
+		// Genius... rocket science...
+		const seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+
+		return seconds * 1000;
+	}
+}
+// There is a lot more fun that can be achieved
+// Need to add socket puppetry (VPNs solutions? several accounts?) - :D
+// TODO: Need to accept automatically permissions except for
+// TODO: Need to add ANN for fake pics
+// TODO: Need to add RNN for fake messages
+
+function getRandomPeriod() {
+	return Math.round(Math.random() * (2000 - 500)) + 500;
+}
+
+(function loopSasori() {
+	// A random period between 500ms and 2secs
+	let randomPeriod = getRandomPeriod();
+
+	setTimeout(function () {
+		randomPeriod = undefined;
+
+		if (checkTinder()) {
+			const delay = trickTinder();
+
+			if (delay) {
+				console.log('Too many likes for now, have to wait: ' + delay + ' ms');
+				randomPeriod = delay;
+			}
+		} else if (checkOkCupid()) {
+			trickOkCupid();
+		}
+
+		if (!randomPeriod) {
+			loopSasori();
+		} else {
+			setTimeout(loopSasori, randomPeriod);
+		}
+	}, randomPeriod);
+}());
